214aba2c4c22b76c1f73813df2d554401c5dc15b
Pawel Borkar <36134699+pawelborkar@users.noreply.github.com>
2023-03-25 01:30:44 +0530

    Develop (#12)
    * add: blogs
    
    * remove: blogPosts from dependency array
    
    * Add: projects
    
    * Fix: no exported member 'Resources' which caused the build to fail
    
    * update: project card ui (#9)
    
    * Feature/projects (#11)
    
    * update: project card ui
    
    * Update: Blog Card UI

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -1,19 +1,15 @@
 import { useAtom } from 'jotai';
-import { Card, Grid, Image, Text } from '@geist-ui/core';
+import { Button, Card, Grid, Image, Text } from '@geist-ui/core';
 import { darkModeToggleAtom } from '@/globalStates';
-
-interface IBlogDetails {
-  id: number;
-  title: string;
-  cover_image: string;
-  published_at: string;
-}
+import { IBlogDetails } from '../Blog/Blog';
+import Link from 'next/link';
 
 const BlogCard = ({
   id,
   title,
+  url,
   cover_image: imageSrc,
-  published_at: publishedDate,
+  readable_publish_date: publishedDate,
 }: IBlogDetails) => {
   const [isDark] = useAtom(darkModeToggleAtom);
   return (
@@ -24,12 +20,26 @@ const BlogCard = ({
       marginBottom={1}
       type={isDark ? 'secondary' : 'default'}
     >
-      <Image src={imageSrc} alt={title} />
+      {/* <Image src={imageSrc} alt={title} /> */}
       <Text h4 my={0}>
         {title}
       </Text>
       <Grid>
-        <Text>{publishedDate}</Text>
+        <Text>Published On: {publishedDate}</Text>
+      </Grid>
+      <Grid>
+        <Link href={url}>
+          <Button
+            style={{
+              border: 'none',
+              color: '#ffffff',
+              backgroundColor: '#0070f3',
+              boxShadow: '0 4px 14px 0 rgba(0,118,255,0.39)',
+            }}
+          >
+            Read
+          </Button>
+        </Link>
       </Grid>
     </Card>
   );
