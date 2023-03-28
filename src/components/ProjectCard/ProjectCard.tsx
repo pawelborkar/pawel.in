e717499203d3dc39cc025889df81fa2faf1e2fee
Pawel Borkar <pawel@duck.com>
2023-03-29 01:36:20 +0530

    update: reduced projectcard width

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -24,7 +24,7 @@ const ProjectCard = ({
         backgroundColor: '#ffff',
         maxWidth:'600px',
         margin:'8px 8px 12px 0px',
-        minWidth: '300px'
+        minWidth: '290px'
       }}
     >
       <Text h4 my={0}>
