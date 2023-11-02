f5732e69956bb25a667913dbcb9fb9ee5cb8c37c
Pawel Borkar <pawel@duck.com>
2023-11-02 17:45:08 +0530

    refactor: Navbar and filenames

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 import { siteConfig } from '@/config/site';
 import { fontSans } from '@/config/fonts';
 import { Providers } from './providers';
-import { Navbar } from '@/components/navbar';
+import { Navbar } from '@/components/Navbar';
 import { Analytics } from '@vercel/analytics/react';
 import clsx from 'clsx';
 
