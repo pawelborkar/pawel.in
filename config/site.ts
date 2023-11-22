c7af6836e1457395231d701cb89d3fd9c98af152
Pawel Borkar <pawel@duck.com>
2023-11-22 17:44:42 +0530

    feat: using URL shortener for tracking clicks

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,7 @@
-// import bootcamper from 'public/assets/bootcamper-logo.png';
 import bootcamper from 'public/assets/bootcamper-logo.png';
 import cryptoHub from 'public/assets/cryptohub-logo.png';
 import jellyfish from 'public/assets/jellyfish.png';
 import doodle from 'public/assets/doodle.ico';
-import pawel from 'public/assets/pawel.jpeg';
 import socialLinks from 'public/assets/links-pawel.png';
 import wikisearch from 'public/assets/wiki-logo.png';
 
@@ -49,9 +47,8 @@ export const siteConfig = {
     {
       id: '01',
       name: 'CryptoHub',
-      description:
-        'CryptoHub: Get all information about cryptocurrencies at one place.',
-      url: 'cryptohub.pawel.in',
+      description: 'CryptoHub: Get all information about cryptocurrencies at one place.',
+      url: 'go.pawel.in/cryptohub',
       git: 'https://github.com/pawelborkar/cryptohub',
       logo: cryptoHub,
     },
@@ -59,7 +56,7 @@ export const siteConfig = {
       id: '02',
       name: 'WikiSearch',
       description: "Search functionality implemented using Wikipedia's API.",
-      url: 'wikisearch.pawel.in',
+      url: 'go.pawel.in/wikisearch',
       git: 'https://github.com/pawelborkar/wikisearch',
       logo: wikisearch,
     },
@@ -67,7 +64,7 @@ export const siteConfig = {
       id: '03',
       name: 'JellyFish Theme',
       description: 'JellyFish: Theme your code editor will love.',
-      url: 'tinyurl.com/je11yfish',
+      url: 'go.pawel.in/jellyfish',
       git: 'https://github.com/pawelborkar/vscode-jellyfish',
       logo: jellyfish,
     },
@@ -75,7 +72,7 @@ export const siteConfig = {
       id: '04',
       name: 'Bootcamper',
       description: 'Add, Review and Find bootcamps in your country.',
-      url: 'api.bootcamper.pawel.in/docs',
+      url: 'go.pawel.in/bootcamper',
       git: 'https://github.com/pawelborkar/bootcamper/docs',
       logo: bootcamper,
     },
@@ -83,16 +80,15 @@ export const siteConfig = {
       id: '05',
       name: 'Doodle',
       description: "Doodle: Drawing pad you'll gonna love.",
-      url: 'pen.pawel.in',
+      url: 'go.pawel.in/doodle',
       git: 'https://github.com/pawelborkar/doodle',
       logo: doodle,
     },
     {
       id: '06',
       name: 'My Social Links',
-      description:
-        'My self made linktree like social links aggregator with added tracking.',
-      url: 'hi.pawel.in',
+      description: 'My self made linktree like social links aggregator with added tracking.',
+      url: 'go.pawel.in/social',
       git: 'https://github.com/pawelborkar/links.pawel.in',
       logo: socialLinks,
     },
@@ -120,7 +116,8 @@ export const siteConfig = {
     },
   ],
   blogDetails: {
-    avatarUrl: 'https://media.licdn.com/dms/image/C5603AQEEyqfszoVe5g/profile-displayphoto-shrink_400_400/0/1657829032658?e=1704326400&v=beta&t=XPn71DuPMPXvOrA32CImJgpHgkWv161Ts3jptaNfAZA',
+    avatarUrl:
+      'https://media.licdn.com/dms/image/C5603AQEEyqfszoVe5g/profile-displayphoto-shrink_400_400/0/1657829032658?e=1704326400&v=beta&t=XPn71DuPMPXvOrA32CImJgpHgkWv161Ts3jptaNfAZA',
     author: 'Pawel',
     twitter: 'https://x.com/pawelsb',
     username: 'pawelsb',
